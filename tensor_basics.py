fa8944ab3e388bc21ff549e3e1990136bffb7865
Jainil Rana <96052787+JNR-10@users.noreply.github.com>
2024-02-29 13:36:32 +0530

    added tensor indexing

--- tensor_basics.py
+++ tensor_basics.py
@@ -145,3 +145,44 @@ z = torch.any(x)
 z = torch.all(
     x
 )
+
+"""
+* Tensor Indexing
+"""
+
+batch_size = 10
+features = 25
+x = torch.rand((batch_size, features))
+
+print(x[0].shape)
+
+print(x[:, 0].shape)
+
+print(x[2, 0:10].shape)
+
+x[0, 0] = 100
+
+x = torch.arange(10)
+indices = [2, 5, 8]
+print(x[indices])
+
+x = torch.rand((3, 5))
+rows = torch.tensor([1, 0])
+cols = torch.tensor([4, 0])
+print(x[rows, cols]) 
+
+x = torch.arange(10)
+print(x[(x < 2) | (x > 8)])  # will be [0, 1, 9]
+print(x[x.remainder(2) == 0])
+
+print(
+    torch.where(x > 5, x, x * 2)
+)
+x = torch.tensor([0, 0, 1, 2, 2, 3, 4]).unique()
+print(
+    x.ndimension()
+) 
+x = torch.arange(10)
+print(
+    x.numel()
+)
