c7f1edb65941f5f341ed7e77b77aafd7dc731358
Jainil Rana <96052787+JNR-10@users.noreply.github.com>
2024-02-29 13:39:44 +0530

    added tensor reshaping

--- tensor_basics.py
+++ tensor_basics.py
@@ -186,3 +186,47 @@ x = torch.arange(10)
 print(
     x.numel()
 )
+
+"""
+* Tensor Reshaping
+"""
+
+x = torch.arange(9)
+
+x_3x3 = x.view(3, 3)
+
+x_3x3 = x.reshape(3, 3)
+
+y = x_3x3.t()
+print(
+    y.is_contiguous()
+)
+print(y.contiguous().view(9))
+
+x1 = torch.rand(2, 5)
+x2 = torch.rand(2, 5)
+print(torch.cat((x1, x2), dim=0).shape)
+print(torch.cat((x1, x2), dim=1).shape)
+
+z = x1.view(-1)
+
+batch = 64
+x = torch.rand((batch, 2, 5))
+z = x.view(
+    batch, -1
+)
+
+z = x.permute(0, 2, 1)
+
+z = torch.chunk(x, chunks=2, dim=1)
+print(z[0].shape)
+print(z[1].shape)
+
+x = torch.arange(
+    10
+)
+print(x.unsqueeze(0).shape)
+print(x.unsqueeze(1).shape)
+
+x = torch.arange(10).unsqueeze(0).unsqueeze(1)
+z = x.squeeze(1)
