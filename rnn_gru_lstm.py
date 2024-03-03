98c0371d3836f0221425f349bd087e7c2dde6c80
Jainil Rana <96052787+JNR-10@users.noreply.github.com>
2024-03-03 13:19:49 +0530

    added LSTM

--- rnn_gru_lstm.py
+++ rnn_gru_lstm.py
@@ -62,7 +62,31 @@ class RNN_GRU(nn.Module):
         # Decode the hidden state of the last time step
         out = self.fc(out)
         return out
-        
+
+# Recurrent neural network with LSTM (many-to-one)
+class RNN_LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(RNN_LSTM, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size * sequence_length, num_classes)
+
+    def forward(self, x):
+        # Set initial hidden and cell states
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+
+        # Forward propagate LSTM
+        out, _ = self.lstm(
+            x, (h0, c0)
+        )  # out: tensor of shape (batch_size, seq_length, hidden_size)
+        out = out.reshape(out.shape[0], -1)
+
+        # Decode the hidden state of the last time step
+        out = self.fc(out)
+        return out
+            
 # Load Data
 train_dataset = datasets.MNIST(
     root="dataset/", train=True, transform=transforms.ToTensor(), download=True
