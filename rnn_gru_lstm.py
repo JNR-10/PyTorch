2ae18ba251ee09f817a59d2652d58444d51a6acb
Jainil Rana <96052787+JNR-10@users.noreply.github.com>
2024-03-03 13:59:36 +0530

    added network training and evaluation code

--- rnn_gru_lstm.py
+++ rnn_gru_lstm.py
@@ -18,11 +18,13 @@ from tqdm import tqdm  # For a nice progress bar!
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # ! Hyperparameters
-input_size = 28
+input_size = 28 # we see this as having 28 time-sequences and each seq has 28 features
+# ? that is how we can think RNN working in this case
 hidden_size = 256
 num_layers = 2
 num_classes = 10
-sequence_length = 28
+sequence_length = 28 # so essentially we are taking one row at a time and sending each row at each time-stamp
+# ? we do not need to specify this, it is just for clarity
 learning_rate = 0.005
 batch_size = 64
 num_epochs = 3
@@ -33,12 +35,18 @@ class RNN(nn.Module):
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
+        # ? MNIST dataset would have batches as the first axis (mini_batch, channel, dim, dim)
+        # ? If True, then the input and output tensors are provided as (batch, seq, feature) instead of (seq, batch, feature).
         self.fc = nn.Linear(hidden_size * sequence_length, num_classes)
         
     def forward(self, x):
+        # Set initial hidden and cell states
         h0 = torch.zeros(self.num_layers, x.size[0], self.hidden_size).to(device)
+        # Forward prop
         out, _ = self.rnn(x, h0)
-        out = out.reshape(out.shape[0], -1)
+        # ? the output '_' will be the hidden state, but we are not going to store the hidden state
+        # ? as every example has it's own hidden state
+        out = out.reshape(out.shape[0], -1) # flattened layer would be 28 x 256 (ie. seq_length x hidden_size)
         out = self.fc(out)
         return out
 
@@ -103,3 +111,49 @@ model = RNN(input_size, hidden_size, num_layers, num_classes).to(device)
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+# Train Network
+for epoch in range(num_epochs):
+    for batch_idx, (data, targets) in enumerate(tqdm(train_loader)):
+        # Get data to cuda if possible
+        data = data.to(device=device).squeeze(1)
+        # ? RNN expects the following kind of input (64, 28, 28)
+        # ? so squeeze function would remove the 1 (ie. the channel)
+        targets = targets.to(device=device)
+
+        # forward
+        scores = model(data)
+        loss = criterion(scores, targets)
+
+        # backward
+        optimizer.zero_grad()
+        loss.backward()
+
+        # gradient descent update step/adam step
+        optimizer.step()
+
+# Check accuracy on training & test to see how good our model
+def check_accuracy(loader, model):
+    num_correct = 0
+    num_samples = 0
+
+    # Set model to eval
+    model.eval()
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device).squeeze(1)
+            y = y.to(device=device)
+
+            scores = model(x)
+            _, predictions = scores.max(1)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+
+    # Toggle model back to train
+    model.train()
+    return num_correct / num_samples
+
+
+print(f"Accuracy on training set: {check_accuracy(train_loader, model)*100:2f}")
+print(f"Accuracy on test set: {check_accuracy(test_loader, model)*100:.2f}")
